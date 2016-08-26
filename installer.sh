f361b691b198ddc2416fae158a1301842873b751
Tymoteusz Paul <tymoteusz.paul@x20x.co.uk>
2016-08-26 17:57:32 +0100

    Updating kibana to 4.5 and logstash to 2.3 branches.

--- installer.sh
+++ installer.sh
@@ -24,7 +24,7 @@ sudo update-rc.d elasticsearch defaults 95 10
 sudo service elasticsearch start
 
 # Kibana
-echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
+echo "deb http://packages.elastic.co/kibana/4.5/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.5.x.list
 sudo apt-get update
 sudo apt-get -y install kibana
 sudo cp /opt/screeps-elk/kibana.yml /opt/kibana/config/kibana.yml
@@ -32,7 +32,7 @@ sudo update-rc.d kibana defaults 96 9
 sudo service kibana restart
 
 # Logstash
-echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.2.x.list
+echo 'deb http://packages.elastic.co/logstash/2.3/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.3.x.list
 sudo apt-get update
 sudo apt-get -y install logstash
 sudo cp /opt/screeps-elk/etc/logstash/conf.d/* /etc/logstash/conf.d/
